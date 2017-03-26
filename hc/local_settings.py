32b16fa6f54ce159f69d54f1f40ae75188c1a6e7
Pēteris Caune <cuu508@gmail.com>
2017-03-26 14:20:40 +0300

    Configure more stuff through environment variables.

--- hc/local_settings.py
+++ hc/local_settings.py
@@ -1,17 +1,24 @@
-"""
-Local settings for the HealthChecks app
-"""
+""" Read local settings from environment variables. """
 
 import os
 
 
-ALLOWED_HOSTS = os.getenv('HC_ALLOWED_HOSTS', '*').split(",")
 DEBUG = os.getenv('HC_DEBUG', False)
+# If HC_SECRET_KEY is absent then use a temporary random one
+SECRET_KEY = os.getenv("HC_SECRET_KEY", os.urandom(20).encode("hex"))
 
-HOST = os.getenv('HC_HOST', "localhost")
-SITE_ROOT = os.getenv('HC_SITE_ROOT', "http://localhost:9090")
+# HTTP stuff
+ALLOWED_HOSTS = os.getenv('HC_ALLOWED_HOSTS', '*').split(",")
+SESSION_COOKIE_SECURE = "HC_SESSION_COOKIE_SECURE" in os.environ
+CSRF_COOKIE_SECURE = "HC_CSRF_COOKIE_SECURE" in os.environ
+SITE_ROOT = os.getenv('HC_SITE_ROOT', "http://localhost:5000")
 PING_ENDPOINT = os.getenv('HC_PING_ENDPOINT', SITE_ROOT + "/ping/")
+PING_EMAIL_DOMAIN = os.getenv("HC_PING_ENMAIL_DOMAIN", "localhost")
 
+# Generate .gz files in CACHE directory.
+COMPRESS_STORAGE = 'compressor.storage.GzipCompressorFileStorage'
+
+# Email
 DEFAULT_FROM_EMAIL = os.getenv('HC_EMAIL_FROM', "healthchecks@example.org")
 EMAIL_HOST = os.getenv('HC_EMAIL_HOST', "localhost")
 EMAIL_PORT = int(os.getenv('HC_EMAIL_PORT', 25))
@@ -19,6 +26,7 @@ EMAIL_HOST_USER = os.getenv('HC_EMAIL_USER', "")
 EMAIL_HOST_PASSWORD = os.getenv('HC_EMAIL_PASSWORD', "")
 EMAIL_USE_TLS = EMAIL_PORT == 587
 
+# Database
 if os.environ.get("HC_DB") == "postgres":
     DATABASES = {
         'default': {
@@ -44,7 +52,49 @@ if os.environ.get("HC_DB") == "mysql":
         }
     }
 
-COMPRESS_STORAGE = 'compressor.storage.GzipCompressorFileStorage'
+# Sentry
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.humanize',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
 
-if "HC_SENTRY_DSN" in os.environ:
-    RAVEN_CONFIG = {"dsn": os.getenv("HC_SENTRY_DSN")}
+    'compressor',
+    'raven.contrib.django.raven_compat',  # <-- just adding this
+
+    'hc.accounts',
+    'hc.api',
+    'hc.front',
+    'hc.payments'
+)
+RAVEN_CONFIG = {"dsn": os.getenv("HC_SENTRY_DSN")}
+
+# Slack
+SLACK_CLIENT_ID = os.getenv("HC_SLACK_CLIENT_ID")
+SLACK_CLIENT_SECRET = os.getenv("HC_SLACK_CLIENT_SECRET")
+
+# Pushover
+PUSHOVER_API_TOKEN = os.getenv("HC_PUSHOVER_API_TOKEN")
+PUSHOVER_SUBSCRIPTION_URL = os.getenv("HC_PUSHOVER_SUBSCRIPTION_URL")
+
+# Pushbullet
+PUSHBULLET_CLIENT_ID = os.getenv("HC_PUSHBULLET_CLIENT_ID")
+PUSHBULLET_CLIENT_SECRET = os.getenv("HC_PUSHBULLET_CLIENT_SECRET")
+
+# Discord
+DISCORD_CLIENT_ID = os.getenv("HC_DISCORD_CLIENT_ID")
+DISCORD_CLIENT_SECRET = os.getenv("HC_DISCORD_CLIENT_SECRET")
+
+# Braintree
+USE_PAYMENTS = "HC_USE_PAYMENTS" in os.environ
+if USE_PAYMENTS:
+    import braintree
+    braintree.Configuration.configure(**{
+        "environment": braintree.Environment.Production,
+        "merchant_id": os.getenv("HC_BRAINTREE_MERCHANT_ID"),
+        "public_key": os.getenv("HC_BRAINTREE_PUBLIC_KEY"),
+        "private_key": os.getenv("HC_BRAINTREE_PRIVATE_KEY")
+    })
